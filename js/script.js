c701fbd6065f29a1de35f34417e7ce596c7a3092
amberuhhh <amberkopas12@gmail.com>
2024-02-05 10:17:52 -0500

    YUP!!!

---
+++ js/script.js
@@ -0,0 +1,113 @@
+console.log("Hello World!");
+
+var client = contentful.createClient({
+    space: 'jhroq6kjrsy4',
+    accessToken: 'zV8UhcSE-aekfCyefr5xPGasK30EwPOOTWfm6RBxnt4',
+  });
+
+var placeForContent = document.getElementById('place-for-content');
+
+
+  // gets all the entries as a json
+  client.getEntries().then(function (entries) {
+    console.log("entries:");
+    console.log(entries);
+    // loops through the json to look at one entry at a time
+    entries.items.forEach(function (entry) {
+        console.log("entry:");
+        console.log(entry);
+      // if statement chechs that this field exists  
+      var entryDiv = document.createElement ('div');
+        if (entry.fields.name) {
+
+            var name = document.createElement('h2');
+            name.innerHTML = entry.fields.name;
+            entryDiv.appendChild(name);
+
+            var bookTags = document.createElement('h5');
+            bookTags.innerHTML = entry.fields.bookTags;
+            entryDiv.appendChild(bookTags);
+
+            var cost = document.createElement('h4');
+            cost.innerHTML = entry.fields.cost;
+            entryDiv.appendChild(cost);
+
+            var description = document.createElement('p');
+            description.innerHTML = entry.fields.description;
+            entryDiv.appendChild(description);
+
+            // var link = document.createElement('a')
+            // link.innerHTML = "link to " + entry.fields.name;
+            // link.href= entry.fields.link;
+            // placeForContent.appendChild(link);
+
+            // do whatever with the info in the field
+            console.log(entry.fields.name);
+        }
+            placeForContent.appendChild(entryDiv);
+    });
+  });
+
+//   var students = [
+//                 {name:"Greg", grade:"A", subject:"AI", courses:[
+//                                                 {name:"web Tech"}, {name:"Logo"}
+//                                             ]}, 
+//                 {name:"Alice", grade:"B+", subject:"Marketing", courses:[
+//                                                 {name:"Biz"}, {name:"AI"}
+//                                             ]}
+//             ];
+  
+//   console.log(students[1].courses[0].name);
+
+// // students.forEach(function (student){
+// //     console.log (student.name);
+// // });
+          
+// students.forEach(function (student){
+//   console.log ("student name is " + student.name);
+//   student.courses.forEach(function (course){
+//       console.log("Course is "+ course.name);
+//   })
+// });
+
+
+
+            
+
+//   //Comment
+
+// //   console.log(client);
+
+  
+// // //   client is helper to connect to contentful
+// // //   getEntries is a method to get all entries from contentful
+// // //   entries is an object that contains all the entries
+// // client.getEntries().then(function (entries) {
+// //     console.log(entries);
+// //     // for each entry run the function
+// //     entries.items.forEach(function (entry) {
+// //         //  fields are the fields we named in contentful
+// //     //   if (entry.fields.name) {
+// //         console.log("entry name: " + entry.fields.name);
+// //         console.log("entry link: " + entry.fields.image);
+// //         console.log("entry link: " + entry.fields.cost);
+// //         console.log("entry link: " + entry.fields.description);
+
+// //         if (entry.fields.mainImage){
+// //             console.log("entry image url: " + entry.fields.mainImage.fields.file.url);
+// //         }
+// //     //   }
+// //     });
+// //   });
+
+// // /*
+// //     var cars = [{type:"Fiat", model:"500", color:"white"}, 
+// //                 {type:"Volvo", model:"XC90", color:"black"}]
+// //     // console.log(cars[0]); // to get all info on first car
+// //     // console.log(cars[0].type); // to get type of first car
+// //     // console.log(cars[1].model);
+
+// //     cars.forEach (function (car) {
+// //         console.log("Car Type is " + car.type);
+// //     });
+// // */
